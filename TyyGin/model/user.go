789b9226040e8472ee85c396920e13a5da9ea83e
ubuntu <ubuntu@localhost.localdomain>
2023-04-14 20:32:27 +0800

    添加细节

--- TyyGin/model/user.go
+++ TyyGin/model/user.go
@@ -5,7 +5,8 @@ package model
 
 type User struct {
 	// gorm.Model
-	Username string `json: "username"`
-	Password string `json: "password"`
-	Role     int    `json: "role"`
+	// 注意tag中 key冒号后不能空格
+	Username string `form:"username" json:"username"`
+	Password string `form:"password" json:"password"`
+	Role     int    `form:"role" json:"role"`
 }
