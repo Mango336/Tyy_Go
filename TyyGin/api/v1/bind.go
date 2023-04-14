22a707dfa35c47d239b999bf37bc55f17e11ef9c
ubuntu <ubuntu@localhost.localdomain>
2023-04-14 14:29:20 +0800

    获取url中携带参数的函数

--- TyyGin/api/v1/bind.go
+++ TyyGin/api/v1/bind.go
@@ -1,5 +1,25 @@
 package v1
 
-func GetPathUrl() {
-	
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GetPathUrl 获取path（url）中携带的参数: 返回的都是string类型
+func GetPathUrl(c *gin.Context) {
+	// url为43.142.18.112:9090/show/get/Mango/25
+	// 获取到username= Mango age=25
+	name := c.Param("name") // Param(key string) key为接口中定义的名
+	age, _ := strconv.Atoi(c.Param("age"))
+	c.JSON(http.StatusOK, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
+// GetInfoBind 参数绑定 ShouldBind()能基于请求自动提取JSON、form表单、QueryString的数据 并把值绑定到指定的结构体对象中
+func GetInfoBind() {
+
 }
