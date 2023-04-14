f080ca4c5787d6a7dcd4dbc1cc3ac98a52b7b55f
ubuntu <ubuntu@localhost.localdomain>
2023-04-14 14:27:34 +0800

    代码规范化修改

--- TyyGin/api/v1/show.go
+++ TyyGin/api/v1/show.go
@@ -6,12 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ShowName 简单的展示 向前端响应一个html页面
 func ShowName(c *gin.Context) {
 	c.HTML(http.StatusOK, "showname.tmpl", gin.H{
 		"title": "子路由1--第一个网页",
 		"name":  "Mango",
 	})
 }
+
+// ShowInfo 向前端发送简单的json数据
 func ShowInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "YLM",
