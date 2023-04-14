59d0d434bdfb19cec1f0adba184112bb00383926
ubuntu <ubuntu@localhost.localdomain>
2023-04-14 14:28:26 +0800

    添加获取登录表单数据第二个方法

--- TyyGin/api/v1/login.go
+++ TyyGin/api/v1/login.go
@@ -2,27 +2,45 @@ package v1
 
 import (
 	"TyyGin/model"
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Load Login Web
+// LoadLoginWeb 加载登录页面
 func LoadLoginWeb(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.tmpl", nil)
 }
+
+// LoginPost 获取登录表单的数据
 func LoginPost(c *gin.Context) {
-	username, _ := c.GetPostForm("username")
-	password, _ := c.GetPostForm("password")
-	role := 0
-	var data model.User = model.User{
-		Username: username,
-		Password: password,
-		Role:     role,
+	// 方法一：
+	// username, _ := c.GetPostForm("username")
+	// password, _ := c.GetPostForm("password")
+	// role := 0
+	// var data model.User = model.User{
+	// 	Username: username,
+	// 	Password: password,
+	// 	Role:     role,
+	// }
+	// log.Println(data)
+	// c.HTML(http.StatusOK, "login_post.tmpl", gin.H{
+	// 	"data": data,
+	// })
+
+	// 方法二：
+	var data model.User
+	if err := c.ShouldBind(&data); err == nil {
+		log.Println(data, data.Username, data.Password, data.Role)
+		c.HTML(http.StatusOK, "login_post.tmpl", gin.H{
+			"data": data,
+		})
+	} else {
+		log.Fatal("Could not bind form info...", err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
 	}
-	fmt.Println(data)
-	c.HTML(http.StatusOK, "login_post.tmpl", gin.H{
-		"data": data,
-	})
+
 }
